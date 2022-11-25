47795ca3d44f70e4f8cfc25bebf4a3bb7be62954
YuraFLex <shvalov.yurec@gmail.com>
2022-11-25 11:56:00 +0200

    add

---
+++ src/index.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMobileBtn: document.querySelector('[data-menu-open]'),
+    closeMobileBtn: document.querySelector('[data-menu-close]'),
+    mobile: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMobileBtn.addEventListener('click', toggleMobile);
+  refs.closeMobileBtn.addEventListener('click', toggleMobile);
+
+  function toggleMobile() {
+    refs.mobile.classList.toggle('is-open');
+  }
+})();
