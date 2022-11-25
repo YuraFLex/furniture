953182b7578ab470434d7e7c0f786a122ac8314c
YuraFLex <105123887+YuraFLex@users.noreply.github.com>
2022-11-25 11:38:36 +0000

    Deploying to gh-pages from @ YuraFLex/furniture@e07189fa32cf178f789b842ddacfb71e9dc4fabd 🚀

--- index.2f8c254d.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openMobileBtn:document.querySelector("[data-menu-open]"),closeMobileBtn:document.querySelector("[data-menu-close]"),mobile:document.querySelector("[data-menu]")};function o(){e.mobile.classList.toggle("is-open")}e.openMobileBtn.addEventListener("click",o),e.closeMobileBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.2f8c254d.js.map
--- index.69d1f3c7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMobileBtn:document.querySelector("[data-menu-open]"),closeMobileBtn:document.querySelector("[data-menu-close]"),mobile:document.querySelector("[data-menu]")};function n(){e.mobile.classList.toggle("is-open")}e.openMobileBtn.addEventListener("click",n),e.closeMobileBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.69d1f3c7.js.map
