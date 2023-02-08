1037b5d72df9f98465cd4e99345c018ae901e580
YuraFLex <shvalov.yurec@gmail.com>
2023-02-08 12:42:14 +0200

    add slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
+// Слайдер
+$('.privileg-list').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      },
+    },
+  ],
+});
+
+$('.slider').slick({
+  lazyLoad: 'ondemand',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+
 // Открытие/заыкртиые мобильного меню
 
 (() => {
